5bf4722d361fca6f78a96bd06e6f76702408ee1a
Silvia <Silvia.Taroni@cern.ch>
2017-02-17 10:07:30 +0100

    resetting lin_out_ vector for endcap only

--- SimCalorimetry/EcalTrigPrimAlgos/interface/EcalFenixStrip.h
+++ SimCalorimetry/EcalTrigPrimAlgos/interface/EcalFenixStrip.h
@@ -60,6 +60,7 @@ class EcalFenixStrip {
 
   // data formats for each event
   std::vector<std::vector<int> > lin_out_;
+  std::vector<std::vector<int> > lin_out_temp;
   std::vector<int> add_out_;
   std::vector<int> filt_out_;
   std::vector<int> peak_out_;
@@ -145,7 +146,6 @@ class EcalFenixStrip {
   }
 
  void  process(const edm::EventSetup &setup, std::vector<EEDataFrame> &samples, int nrXtals, std::vector<int> & out){
-   debug_= true;
 // now call processing
    if (samples.size()==0) {
      std::cout<<" Warning: 0 size vector found in EcalFenixStripProcess!!!!!"<<std::endl;
@@ -156,11 +156,14 @@ class EcalFenixStrip {
    
    identif_ = getFGVB()->getMissedStripFlag();
    
-   if (debug_) std::cout<< __PRETTY_FUNCTION__ << " line " << __LINE__ << " processing part1_endcap " << std::endl; 
+   if (debug_) std::cout<< __PRETTY_FUNCTION__ << " line " << __LINE__ << " processing part1_endcap " << ecaltpgFgStripEE_ << std::endl; 
    process_part1(identif_,samples,nrXtals,stripid,ecaltpPed_,ecaltpLin_,ecaltpgWeightMap_,ecaltpgWeightGroup_,ecaltpgBadX_); //templated part
-   if (debug_) std::cout<< __PRETTY_FUNCTION__ << " line " << __LINE__ << " processing part2_endcap " << std::endl; 
+   if (debug_) std::cout<< __PRETTY_FUNCTION__ << " line " << __LINE__ << " processing part2_endcap " << ecaltpgFgStripEE_<< std::endl; 
 
    process_part2_endcap(stripid,ecaltpgSlidW_,ecaltpgFgStripEE_,ecaltpgStripStatus_);
+
+   if (debug_) std::cout<< __PRETTY_FUNCTION__ << " line " << __LINE__ << " part2_endcap processed " << ecaltpgFgStripEE_<< std::endl; 
+  
    out=format_out_; //FIXME: timing
    return;
  }
@@ -169,36 +172,40 @@ class EcalFenixStrip {
  void  process_part1(int identif, std::vector<T> & df,int nrXtals, uint32_t stripid, const EcalTPGPedestals * ecaltpPed, const
  EcalTPGLinearizationConst *ecaltpLin,const EcalTPGWeightIdMap * ecaltpgWeightMap,const EcalTPGWeightGroup * ecaltpgWeightGroup, const EcalTPGCrystalStatus * ecaltpBadX)
    {
-     debug_=true ; 
-  
+
      if(debug_) std::cout<< __PRETTY_FUNCTION__ << " line " << __LINE__ << " EcalFenixStrip input is a vector of size: "<<nrXtals<< std::endl;
 
       //loop over crystals
       for (int cryst=0;cryst<nrXtals;cryst++) {
-	/* if(debug_){ */
-	/*   std::cout<<std::endl; */
-	/*   std::cout <<"cryst= "<<cryst<<" EBDataFrame/EEDataFrame is: "<<std::endl;  */
-	/*   for ( int i = 0; i<df[cryst].size();i++){ */
-	/*     std::cout <<" "<<std::dec<<df[cryst][i].adc(); */
-	/*   } */
-	/*   std::cout<<std::endl; */
-	/* } */
-// call linearizer
+	if(debug_){
+	  std::cout<<std::endl;
+	  std::cout <<"cryst= "<<cryst<<" EBDataFrame/EEDataFrame is: "<<std::endl;
+	  for ( int i = 0; i<df[cryst].size();i++){
+	    std::cout <<" "<<std::dec<<df[cryst][i].adc();
+	  }
+	  std::cout<<std::endl;
+	}
+	// call linearizer
 	this->getLinearizer(cryst)->setParameters(df[cryst].id().rawId(),ecaltpPed,ecaltpLin,ecaltpBadX) ; 
 	this->getLinearizer(cryst)->process(df[cryst],lin_out_[cryst]);
       }
-      for (unsigned int cryst=nrXtals; cryst<lin_out_.size(); cryst++){
-	lin_out_[cryst].assign(lin_out_[cryst].size(), 0);
+      //copying lin_out_ to lin_out_temp
+      lin_out_temp=lin_out_;
+      //Silvia: Setting to 0 all the unused crystals. Otherwise it keeps the previous event value and bias the fgvb result
+      if (debug_) std::cout << __PRETTY_FUNCTION__ << " line "<< __LINE__ << " " << df[0].id().subdetId()<< std::endl;
+      if ((df[0].id().subdetId())==2){//Silvia: Set to 0 all the unused crystals for the Endcap. I don't know the consequences on Barrel, doing it only for Endcap 
+	for (unsigned int cryst=nrXtals; cryst<lin_out_temp.size(); cryst++){
+	  lin_out_temp[cryst].assign(lin_out_temp[cryst].size(), 0);
+	}
       }
-
       if(debug_){
       	std::cout<<  __PRETTY_FUNCTION__ << " " << __LINE__ << "  output of linearizer is a vector of size: "
               <<std::dec<<lin_out_.size()<<" of which used "<<nrXtals<<std::endl;
       	for (int ix=0;ix<nrXtals;ix++){
       	  std::cout<< "cryst: "<<ix<<"  value : "<<std::dec<<std::endl;
-      	  std::cout<<" lin_out[ix].size()= "<<std::dec<<lin_out_[ix].size()<<std::endl;
+      	  std::cout<<" lin_out[ix].size()= "<<std::dec<<lin_out_temp[ix].size()<<std::endl;
       	  for (unsigned int i =0; i<lin_out_[ix].size();i++){
-      	    std::cout <<" "<<std::dec<<(lin_out_[ix])[i];
+      	    std::cout <<" "<<std::dec<<(lin_out_temp[ix])[i];
       	  }
       	  std::cout<<std::endl;
       	}
@@ -207,11 +214,11 @@ class EcalFenixStrip {
       }
  
       // Now call the sFGVB - this is common between EB and EE!
-      if(debug_) std::cout<< __PRETTY_FUNCTION__ << " line " << __LINE__ << " calling fgvb " << std::endl;
+      if(debug_) std::cout<< __PRETTY_FUNCTION__ << " line " << __LINE__ << " calling fgvb " <<ecaltpgFgStripEE_<<  std::endl;
       getFGVB()->setParameters(identif, stripid,ecaltpgFgStripEE_);
       if(debug_) std::cout<< __PRETTY_FUNCTION__ << " line " << __LINE__ << " calling fgvb and processing " << std::endl;
 
-      getFGVB()->process(lin_out_,fgvb_out_temp_);
+      getFGVB()->process(lin_out_temp,fgvb_out_temp_);
 
       if(debug_)
       {
@@ -223,7 +230,7 @@ class EcalFenixStrip {
       }
  
       // call adder
-      this->getAdder()->process(lin_out_,nrXtals,add_out_);  //add_out is of size SIZEMAX=maxNrSamples
+      this->getAdder()->process(lin_out_temp,nrXtals,add_out_);  //add_out is of size SIZEMAX=maxNrSamples
  
       if(debug_){
       	std::cout<<  __PRETTY_FUNCTION__ << " " << __LINE__ << " \n output of adder is a vector of size: "<<std::dec<<add_out_.size()<<std::endl;
@@ -243,29 +250,29 @@ class EcalFenixStrip {
 	this->getFilter()->setParameters(stripid,ecaltpgWeightMap,ecaltpgWeightGroup); 
 	this->getFilter()->process(add_out_,filt_out_,fgvb_out_temp_,fgvb_out_); 
 
-	/* if(debug_){ */
-	/*   std::cout<<  __PRETTY_FUNCTION__ << " " << __LINE__ << " \n output of filter is a vector of size: "<<std::dec<<filt_out_.size()<<std::endl;  */
-	/*   for (unsigned int ix=0;ix<filt_out_.size();ix++){ */
-	/*     std::cout<< "cryst: "<<ix<<"  value : "<<std::dec<<filt_out_[ix]<<std::endl; */
-	/*   } */
-	/*   std::cout<<std::endl; */
+	if(debug_){
+	  std::cout<<  __PRETTY_FUNCTION__ << " " << __LINE__ << " \n output of filter is a vector of size: "<<std::dec<<filt_out_.size()<<std::endl;
+	  for (unsigned int ix=0;ix<filt_out_.size();ix++){
+	    std::cout<< "cryst: "<<ix<<"  value : "<<std::dec<<filt_out_[ix]<<std::endl;
+	  }
+	  std::cout<<std::endl;
 
-        /*   std::cout<<  __PRETTY_FUNCTION__ << " " << __LINE__ << "\n output of sfgvb after filter is a vector of size: "<<std::dec<<fgvb_out_.size()<<std::endl; */
-        /*   for (unsigned int ix=0;ix<fgvb_out_.size();ix++){ */
-        /*     std::cout<< "cryst: "<<ix<<"  value : "<<std::dec<<fgvb_out_[ix]<<std::endl; */
-        /*   } */
-        /*   std::cout<<std::endl; */
-	/* } */
+          std::cout<<  __PRETTY_FUNCTION__ << " " << __LINE__ << "\n output of sfgvb after filter is a vector of size: "<<std::dec<<fgvb_out_.size()<<std::endl;
+          for (unsigned int ix=0;ix<fgvb_out_.size();ix++){
+            std::cout<< "cryst: "<<ix<<"  value : "<<std::dec<<fgvb_out_[ix]<<std::endl;
+          }
+          std::cout<<std::endl;
+	}
 
-	// call peakfinder
+	////call peakfinder
 	this->getPeakFinder()->process(filt_out_,peak_out_);
-	/* if(debug_){ */
-	/*   std::cout<<  __PRETTY_FUNCTION__ << " " << __LINE__ << "\n  output of peakfinder is a vector of size: "<<peak_out_.size()<<std::endl;  */
-	/*   for (unsigned int ix=0;ix<peak_out_.size();ix++){ */
-	/*     std::cout<< "cryst: "<<ix<<"  value : "<<peak_out_[ix]<<std::endl; */
-	/*   } */
-	/*   std::cout<<std::endl; */
-	/* } */
+	if(debug_){
+	  std::cout<<  __PRETTY_FUNCTION__ << " " << __LINE__ << "\n  output of peakfinder is a vector of size: "<<peak_out_.size()<<std::endl;
+	  for (unsigned int ix=0;ix<peak_out_.size();ix++){
+	    std::cout<< "cryst: "<<ix<<"  value : "<<peak_out_[ix]<<std::endl;
+	  }
+	  std::cout<<std::endl;
+	}
 	return;
       }
    }
