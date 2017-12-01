c341b0e48e19bbe5fc84b04ffc214f1e52ddb7c8
Silvia <Silvia.Taroni@cern.ch>
2017-12-01 12:39:36 +0100

    WP and effAreas in cfi file

--- DPGAnalysis/Skims/python/ZElectronsSelector_cfi.py
+++ DPGAnalysis/Skims/python/ZElectronsSelector_cfi.py
@@ -5,8 +5,6 @@ print "ZEE SKIM. RUN ON MINIAOD = ",RUN_ON_MINIAOD
 
 # cuts
 ELECTRON_CUT=("pt > 10 && abs(eta)<2.5")
-DIELECTRON_CUT=("mass > 70 && mass < 110 && daughter(0).pt>20 && daughter(1).pt()>10")
-
 
 # single lepton selectors
 if RUN_ON_MINIAOD:
@@ -20,11 +18,24 @@ else:
                                     cut = cms.string(ELECTRON_CUT)
                                     )
 
+eleIDWP = cms.PSet( #first for barrel, second for endcap. All values from cutBasedElectronID-Summer16-80X-V1-veto
+    full5x5_sigmaIEtaIEtaCut       = cms.vdouble(0.0115 ,0.0370 )  , # full5x5_sigmaIEtaIEtaCut
+    dEtaInSeedCut                  = cms.vdouble(0.00749,0.00895)  , # dEtaInSeedCut
+    dPhiInCut                      = cms.vdouble(0.228  ,0.213  )  , # dPhiInCut
+    hOverECut                      = cms.vdouble(0.356  ,0.211  )  , # hOverECut
+    relCombIsolationWithEALowPtCut = cms.vdouble(0.175  ,0.159  )  , # relCombIsolationWithEALowPtCut
+    absEInverseMinusPInverseCut    = cms.vdouble(0.299  ,0.150  )  , # absEInverseMinusPInverseCut
+    missingHitsCut                 = cms.vint32(2       ,3      )    # missingHitsCut
+) 
+
 
 identifiedElectrons = cms.EDFilter("ZElectronsSelectorAndSkim",
-                         src    = cms.InputTag("goodZeeElectrons"),
-                         effAreasConfigFile= cms.FileInPath("RecoEgamma/ElectronIdentification/data/Summer16/effAreaElectrons_cone03_pfNeuHadronsAndPhotons_80X.txt"), # from https://github.com/ikrav/cmssw/blob/egm_id_80X_v1/RecoEgamma/ElectronIdentification/python/Identification/cutBasedElectronID_Summer16_80X_V1_cff.py#L131
-                         rho = cms.InputTag("fixedGridRhoFastjetCentralCalo") #from https://github.com/cms-sw/cmssw/blob/09c3fce6626f70fd04223e7dacebf0b485f73f54/RecoEgamma/ElectronIdentification/python/Identification/cutBasedElectronID_tools.py#L564
+                                   src    = cms.InputTag("goodZeeElectrons"),
+                                   eleID = eleIDWP, 
+                                   absEtaMin=cms.vdouble( 0.0000, 1.0000, 1.4790, 2.0000, 2.2000, 2.3000, 2.4000),
+                                   absEtaMax=cms.vdouble( 1.0000,  1.4790, 2.0000,  2.2000, 2.3000, 2.4000, 5.0000),
+                                   effectiveAreaValues=cms.vdouble( 0.1703, 0.1715, 0.1213, 0.1230, 0.1635, 0.1937, 0.2393),
+                                   rho = cms.InputTag("fixedGridRhoFastjetCentralCalo") #from https://github.com/cms-sw/cmssw/blob/09c3fce6626f70fd04223e7dacebf0b485f73f54/RecoEgamma/ElectronIdentification/python/Identification/cutBasedElectronID_tools.py#L564
                          )
 DIELECTRON_CUT=("mass > 70 && mass < 110 && daughter(0).pt>20 && daughter(1).pt()>10")
 
--- DPGAnalysis/Skims/src/ZElectronsSelector.cc
+++ DPGAnalysis/Skims/src/ZElectronsSelector.cc
@@ -1,9 +1,11 @@
 #include "FWCore/PluginManager/interface/ModuleDef.h"
 
 // system include files
-#include <memory>
+
 #include <algorithm>
 #include <iostream>
+#include <memory>
+#include <string>
 #include <vector> 
 
 // user include files
@@ -15,7 +17,7 @@
 
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 
-#include "RecoEgamma/EgammaTools/interface/EffectiveAreas.h"
+//#include "RecoEgamma/EgammaTools/interface/EffectiveAreas.h"
 #include "DataFormats/EgammaCandidates/interface/GsfElectron.h"
 #include "DataFormats/EgammaCandidates/interface/GsfElectronFwd.h"
 #include "DataFormats/GsfTrackReco/interface/GsfTrack.h"
@@ -27,7 +29,7 @@
 #include "CommonTools/UtilAlgos/interface/SingleObjectSelector.h"
 #include "DataFormats/Common/interface/View.h"
 
-#include <string>
+
 using namespace std;
 using namespace reco;
 namespace edm { class EventSetup; }
@@ -38,25 +40,84 @@ public:
   ZElectronsSelector(const edm::ParameterSet&, edm::ConsumesCollector & iC );
   bool operator()(const reco::GsfElectron & ) const;
   void newEvent (const edm::Event&, const edm::EventSetup&);
-  EffectiveAreas _effectiveAreas;
-  edm::InputTag rhoTag;
-  //int nSelectedEvents;
+  const float getEffectiveArea(float eta) const;
+  void printEffectiveAreas() const;
 
   edm::EDGetTokenT<double> theRhoToken;
   edm::EDGetTokenT<reco::GsfElectronCollection> theGsfEToken;
   edm::Handle< double > _rhoHandle;
- 
+
+  std::vector<double> absEtaMin_; // low limit of the eta range
+  std::vector<double> absEtaMax_; // upper limit of the eta range
+  std::vector<double> effectiveAreaValues_; // effective area for this eta range
+
+  edm::ParameterSet eleIDWP;
+
+  vector<int> missHits ;
+  vector<double> sigmaIEtaIEtaCut;
+  vector<double> dEtaInSeedCut   ;
+  vector<double> dPhiInCut       ;
+  vector<double> hOverECut       ;
+  vector<double> relCombIso      ;
+  vector<double> absEInverseMinusPInverseCut ;
+
+
 };
 
+
+void ZElectronsSelector::printEffectiveAreas() const {
+ 
+ 
+   printf("  eta_min   eta_max    effective area\n");
+   uint nEtaBins = absEtaMin_.size();
+   for(uint iEta = 0; iEta<nEtaBins; iEta++){
+     printf("  %8.4f    %8.4f   %8.5f\n",
+        absEtaMin_[iEta], absEtaMax_[iEta],
+        effectiveAreaValues_[iEta]);
+   }
+ 
+}
+const float ZElectronsSelector::getEffectiveArea(float eta) const{
+
+  float effArea = 0;
+  uint nEtaBins = absEtaMin_.size();
+  for(uint iEta = 0; iEta<nEtaBins; iEta++){
+    if( std::abs(eta) >= absEtaMin_[iEta]
+    && std::abs(eta) < absEtaMax_[iEta] ){
+      effArea = effectiveAreaValues_[iEta];
+      break;
+    }
+  }
+
+  return effArea;
+}
+
+
 ZElectronsSelector::ZElectronsSelector(const edm::ParameterSet& cfg, edm::ConsumesCollector & iC ) :
-  _effectiveAreas( (cfg.getParameter<edm::FileInPath>("effAreasConfigFile")).fullPath()),
   theRhoToken(iC.consumes <double> (cfg.getParameter<edm::InputTag>("rho")))
-{}
+{
+  absEtaMin_ = cfg.getParameter<std::vector<double> >("absEtaMin");
+  absEtaMax_ = cfg.getParameter<std::vector<double> >("absEtaMax");
+  effectiveAreaValues_= cfg.getParameter<std::vector<double> >("effectiveAreaValues"); 
+  //printEffectiveAreas();
+  eleIDWP = cfg.getParameter<edm::ParameterSet>("eleID");
+
+  missHits = eleIDWP.getParameter<std::vector<int> >("missingHitsCut");
+  sigmaIEtaIEtaCut= eleIDWP.getParameter<std::vector<double> >("full5x5_sigmaIEtaIEtaCut");
+  dEtaInSeedCut   = eleIDWP.getParameter<std::vector<double> >("dEtaInSeedCut");
+  dPhiInCut       = eleIDWP.getParameter<std::vector<double> >("dEtaInSeedCut");
+  hOverECut       = eleIDWP.getParameter<std::vector<double> >("hOverECut");
+  relCombIso      = eleIDWP.getParameter<std::vector<double> >("relCombIsolationWithEALowPtCut");
+  absEInverseMinusPInverseCut = eleIDWP.getParameter<std::vector<double> >("absEInverseMinusPInverseCut");
+
+}
+
+
 
 void  ZElectronsSelector::newEvent(const edm::Event& ev, const edm::EventSetup& ){
 
   ev.getByToken(theRhoToken,_rhoHandle);
-
+  
 }
 
 bool ZElectronsSelector::operator()(const reco::GsfElectron& el) const{
@@ -71,34 +132,34 @@ bool ZElectronsSelector::operator()(const reco::GsfElectron& el) const{
   std::cout << __LINE__ << std::endl;
   
   if (el.isEB()){
-    if (etrack->hitPattern().numberOfLostHits(reco::HitPattern::MISSING_INNER_HITS)>2) return keepEle; 
-    if (el.full5x5_sigmaIetaIeta() >  0.0115) return keepEle; 
-    if (fabs(el.deltaPhiSuperClusterTrackAtVtx())> 0.228) return keepEle; 
-    if (fabs(el.deltaEtaSuperClusterTrackAtVtx())> 0.00749) return keepEle; 
-    if (el.hadronicOverEm()>0.346) return keepEle;
+    if (etrack->hitPattern().numberOfLostHits(reco::HitPattern::MISSING_INNER_HITS)>missHits[0]) return keepEle; 
+    if (el.full5x5_sigmaIetaIeta() >  sigmaIEtaIEtaCut[0]) return keepEle; 
+    if (fabs(el.deltaPhiSuperClusterTrackAtVtx())> dPhiInCut[0]) return keepEle; 
+    if (fabs(el.deltaEtaSuperClusterTrackAtVtx())> dEtaInSeedCut[0]) return keepEle; 
+    if (el.hadronicOverEm()>hOverECut[0]) return keepEle;
     float abseta = fabs((el.superCluster().get())->position().eta());
     if (abseta> 1.479) return keepEle; // check if it is really needed
-    const float  eA = _effectiveAreas.getEffectiveArea( abseta );
+    const float  eA = getEffectiveArea( abseta );
     const float rho = _rhoHandle.isValid() ? (float)(*_rhoHandle) : 0; 
     std::cout << __LINE__ << " rhoHandle.isValid() " << _rhoHandle.isValid()  << std::endl; 
     std::cout << __LINE__ << " ept:" <<  pt_e << ", e eta: "<< eta_e << " " << eA<< " " << rho << std::endl; 
     if (( el.pfIsolationVariables().sumChargedHadronPt + 
 	  std::max(float(0.0), el.pfIsolationVariables().sumNeutralHadronEt +  
 		   el.pfIsolationVariables().sumPhotonEt -  eA*rho )
-	  ) > 0.175*pt_e) return keepEle; 
+	  ) > relCombIso[0]*pt_e) return keepEle; 
     keepEle=true;
     
   }else if (el.isEE()){
-    if (etrack->hitPattern().numberOfLostHits(reco::HitPattern::MISSING_INNER_HITS)>3) return keepEle; 
-    if (el.full5x5_sigmaIetaIeta() > 0.037) return keepEle; 
-    if (fabs(el.deltaPhiSuperClusterTrackAtVtx())> 0.213) return keepEle; 
-    if (fabs(el.deltaEtaSuperClusterTrackAtVtx())> 0.00895) return keepEle; 
-    if (el.hadronicOverEm()>0.211) return keepEle;
+    if (etrack->hitPattern().numberOfLostHits(reco::HitPattern::MISSING_INNER_HITS)>missHits[1]) return keepEle; 
+    if (el.full5x5_sigmaIetaIeta() >  sigmaIEtaIEtaCut[1]) return keepEle; 
+    if (fabs(el.deltaPhiSuperClusterTrackAtVtx())> dPhiInCut[1]) return keepEle; 
+    if (fabs(el.deltaEtaSuperClusterTrackAtVtx())> dEtaInSeedCut[1]) return keepEle; 
+    if (el.hadronicOverEm()>hOverECut[1]) return keepEle;
     float abseta = fabs((el.superCluster())->position().eta());
     if (abseta< 1.479) return keepEle; // check if it is really needed
     if (abseta>=2.5  ) return keepEle; // check if it is really needed
     
-    const float  eA = _effectiveAreas.getEffectiveArea( abseta );
+    const float  eA = getEffectiveArea( abseta );
     const float rho = _rhoHandle.isValid() ? (float)(*_rhoHandle) : 0; 
     std::cout << __LINE__ << " rhoHandle.isValid() " << _rhoHandle.isValid()  << std::endl; 
     std::cout << __LINE__ << " ept:" <<  pt_e << ", e eta: "<< eta_e << " " << eA<< " " << rho << std::endl; 
@@ -106,7 +167,7 @@ bool ZElectronsSelector::operator()(const reco::GsfElectron& el) const{
     if ((el.pfIsolationVariables().sumChargedHadronPt + 
 	 std::max(float(0.0), el.pfIsolationVariables().sumNeutralHadronEt + 
 		 el.pfIsolationVariables().sumPhotonEt - eA*rho)
-	 ) >0.159*pt_e) return keepEle; 
+	 ) >relCombIso[1]*pt_e) return keepEle; 
     keepEle=true;
 	
   }//isEE
@@ -123,7 +184,6 @@ EVENTSETUP_STD_INIT(ZElectronsSelector);
 typedef SingleObjectSelector<
   edm::View<reco::GsfElectron>,
   ZElectronsSelector
-  //  AndSelector< ZElectronsSelector,   StringCutObjectSelector<reco::GsfElectron> >
 > ZElectronsSelectorAndSkim;
 
 
